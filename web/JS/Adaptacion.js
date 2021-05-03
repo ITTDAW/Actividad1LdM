a4172af3a676b16818422070915514c5ad72f6b7
CARWENC <carwenblue@gmail.com>
2021-05-03 15:36:05 +0200

    incluyo JQuery y css para hacer animado el icono menú hamburguesa, cambio el html de menumovil

--- web/JS/Adaptacion.js
+++ web/JS/Adaptacion.js
@@ -1,6 +1,14 @@
 
-// Mediante la siguiente secuencia, ponemos el boton a la escucha de que sea pulsado, y segun se pulse
-// seleccionamos el menu y le llamamos clase show
+// Mediante la siguiente secuencia JAVASCRIPT, ponemos el boton a la escucha de que sea pulsado, y segun se pulse
+// seleccionamos el menu y le llamamos clase show.
 document.querySelector(".menumovil").addEventListener("click", () => {
     document.querySelector(".menu").classList.toggle("show");
   });
+
+  //JQUERY para hacer la hamburguesa animada. Instalamos la libreria con un script en el head, y damos la siguiente orden.
+  $(document).ready(function(){
+    $(".menumovil").click(function(){
+      $(".hamburguesa").toggleClass("cruz")
+    })
+  })
+
